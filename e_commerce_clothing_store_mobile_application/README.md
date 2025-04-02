2361e434d2aa8fbc5b0eb8c6b805bfe18ad64a2b
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-04-02 18:54:47 +0300

    initialization

---
+++ e_commerce_clothing_store_mobile_application/README.md
@@ -0,0 +1,3 @@
+# e_commerce_clothing_store_mobile_application
+
+A new Flutter project.
---
+++ e_commerce_clothing_store_mobile_application/android/app/src/main/kotlin/com/example/e_commerce_clothing_store_mobile_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.e_commerce_clothing_store_mobile_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
