53825c298159b5d2728e3a78f61d64260d5152d1
haymi <haymanotaweke9@gmail.com>
2025-04-09 08:52:39 +0300

    Integrate API with product list and product details page

--- e_commerce_clothing_store_mobile_application/lib/main.dart
+++ e_commerce_clothing_store_mobile_application/lib/main.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'features/products/presentation/pages/product_details_page.dart';
+
+import 'features/products/presentation/product_list_page.dart';
 
 
 void main() {
@@ -11,10 +14,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ProductDetailsPage(),
+        body: ProductListPage(),
+
       ),
     );
   }
